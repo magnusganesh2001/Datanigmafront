f444dfb8c1018e533a5a9620698008de6b755f6f
JerubSinghSamuel <jerubsingh@gmail.com>
2022-11-19 16:36:58 +0530

    Applied candidates view for employer

--- src/app/applied-candidates/applied-candidates.component.ts
+++ src/app/applied-candidates/applied-candidates.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/services/auth.service';
+import { JobService } from '../core/services/job.service';
 
 @Component({
   selector: 'app-applied-candidates',
@@ -8,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class AppliedCandidatesComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'phone', 'email', 'resume'];
-  dataSource = [];
+  dataSource: { name: any; phone: any; email: any; resume: any; }[] = [];
 
-  constructor() { }
+  constructor(private jobService: JobService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    let user = this.authService.getTokenData();
+    console.log(user);
+    this.jobService.getCandidates().then(res => {
+      res.data.candidates.forEach((e: { name: any; phone: any; email: any; resume: any }) => {
+        this.dataSource.push({
+          'name': e.name,
+          'phone': e.phone,
+          'email': e.email,
+          'resume': e.resume
+        });
+      });
+      console.log(this.dataSource);
+      
+    });
   }
 
 }
